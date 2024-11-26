b35d63f6ee02c02949156a0eaec63d9ffea47263
aymanebs <aymane.bisdaoune@gmail.com>
2024-11-26 12:20:48 +0100

    feat(auth): implement authentication and registration with Passport
    - Set up Passport with Local and JWT strategies for authentication
    - Created user registration functionality
    - Integrated Passport for handling user login and generating JWT tokens
    - Added guards and decorators for protecting routes and managing public access

---
+++ src/auth/dtos/register.dto.ts
@@ -0,0 +1,21 @@
+import { Exclude } from "class-transformer";
+import { IsEmail, IsString, MaxLength, MinLength} from "class-validator";
+
+export class RegisterDto{
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
+    first_name:string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
+    last_name:string;
+    
+    @IsEmail()
+    email:string;
+
+    @IsString()
+    @MinLength(8)
+    password:string;
+}
